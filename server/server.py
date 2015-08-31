b59c55a7c35875ec87713aad7be5a0159bdbb377
Matt Memmesheimer <m.memmesheimer@gmail.com>
2015-08-31 00:05:12 +0000

    Adding basic HTTP server implementation and driver.

--- server/server.py
+++ server/server.py
@@ -1,3 +1,14 @@
-import RPi.GPIO as GPIO
+import BaseHTTPServer
 
-print "Hello, world."
+class HttpServer(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_HEAD(s):
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.end_headers()
+    def do_GET(s):
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.end_headers()
+        s.wfile.write("<html><head><title>Server Test</title></head>")
+        s.wfile.write("<body><h1>Server Test</h1><p>Your server is working.</p>")
+        s.wfile.write("</body></html>")
