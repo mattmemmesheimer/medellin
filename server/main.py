e24711d63ac36278fdd38b965744dee3909acbd8
Matt Memmesheimer <m.memmesheimer@gmail.com>
2015-09-02 00:11:18 +0000

    Adding GPIO pin to server.

--- server/main.py
+++ server/main.py
@@ -1,23 +1,18 @@
 import argparse
 import time
-import BaseHTTPServer
-from server import HttpServer
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Starts up the server.')
     parser.add_argument('host_address', metavar='HOST ADDRESS', help='host address')
     parser.add_argument('-p', '--p', metavar='HOST PORT', help='host port',\
         nargs='?', default='8080', dest='host_port', type=int)
+    parser.add_argument('-g', '--g', metavar='GPIO PIN', help='GPIO pin',\
+        nargs='?', default='4', dest='gpio_pin', type=int)
     args = parser.parse_args()
     
     host = args.host_address
     port = args.host_port
-    server_class = BaseHTTPServer.HTTPServer
-    httpd = server_class((host, port), HttpServer)
+    server = HttpServer((host, port), args.gpio_pin)
     print time.asctime(), "Server Starts - %s:%s" % (host, port)
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    httpd.server_close()
+    server.start()
     print time.asctime(), "Server Stops - %s:%s" % (host, port)
--- server/server.py
+++ server/server.py
@@ -1,14 +1,26 @@
 import BaseHTTPServer
 
-class HttpServer(BaseHTTPServer.BaseHTTPRequestHandler):
-    def do_HEAD(s):
-        s.send_response(200)
-        s.send_header("Content-type", "text/html")
-        s.end_headers()
-    def do_GET(s):
-        s.send_response(200)
-        s.send_header("Content-type", "text/html")
-        s.end_headers()
-        s.wfile.write("<html><head><title>Server Test</title></head>")
-        s.wfile.write("<body><h1>Server Test</h1><p>Your server is working.</p>")
-        s.wfile.write("</body></html>")
+class HttpServer:
+    def __init__(self, server_address, gpio_pin):
+        HttpServerHandler.gpio_pin = gpio_pin
+        self.server_address = server_address
+    def start(self):
+        server = BaseHTTPServer.HTTPServer(self.server_address, HttpServerHandler)
+        try:
+            server.serve_forever()
+        except KeyboardInterrupt:
+            pass
+
+class HttpServerHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write("<html><head><title>Server Test</title></head>")
+        self.wfile.write("<body><h1>Server Test</h1><p>Your server is working.</p>")
+        self.wfile.write("<b>Pin: </b>" + str(self.gpio_pin))
+        self.wfile.write("</body></html>")
