a6ce27e88097edceed0f7a5b99dbc0c7a6fb32e2
Matt Memmesheimer <m.memmesheimer@gmail.com>
2015-09-05 05:37:10 +0000

    Adding GPIO input support.

--- client/client.py
+++ client/client.py
@@ -1,5 +1,21 @@
 import RPi.GPIO as GPIO
+import time
 
 class Client:
-    def __init__(self, server_address):
+    def __init__(self, server_address, gpio_pin):
         self.server_address = server_address
+        self.gpio_pin = gpio_pin
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(gpio_pin, GPIO.IN)
+    def start(self):
+        try:
+            while 1:
+                if GPIO.input(self.gpio_pin):
+                    # send off network call
+                    print('Sending network request...')
+                else:
+                    time.sleep(0.25) 
+        except KeyboardInterrupt:
+            pass
+    def stop(self):
+        GPIO.cleanup()    
--- client/main.py
+++ client/main.py
@@ -1,4 +1,5 @@
 import argparse
+import time
 from client import Client
 
 if __name__ == '__main__':
@@ -6,6 +7,12 @@ if __name__ == '__main__':
     parser.add_argument('server_address', metavar='SERVER ADDRESS', help='server address')
     parser.add_argument('-p', '--port', metavar='SERVER PORT', help='server port',\
         nargs='?', default='8080', dest='server_port', type=int)
+    parser.add_argument('-g', '--gpio', metavar='GPIO PIN', help='GPIO pin',\
+        nargs='?', default='4', dest='gpio_pin', type=int)
     
     args = parser.parse_args()
-    client = Client((args.server_address, args.server_port))
+    client = Client((args.server_address, args.server_port), args.gpio_pin)
+    print time.asctime(), 'Client Starts - %s:%s' % (args.server_address, args.server_port)
+    client.start()
+    print time.asctime(), 'Client Stops'
+    client.stop()
